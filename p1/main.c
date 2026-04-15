9eeec089fd826ec5225a913e915c5e9f99140de4
2025050214 <jeejee637@kau.kr>
2026-04-15 15:17:53 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main () {
+  int answer;
+  int num;
+  int count = 0;
+
+  scanf("%d", &answer);
+
+  do {
+    scanf("%d", &num);
+    count++;
+
+    if (num > answer) {
+      printf("%d>?\n", num);
+      printf("%d보다 낮습니다\n", num);
+    }
+    else if (num < answer) {
+      printf("%d<?\n", num);
+      printf("%d보다 높습니다\n", num);
+    }
+    else {
+      printf("%d==?\n", num);
+      printf("%d 정답입니다\n", num);
+    }
+  } while (num != answer);
+  printf("%d -> 시도횟수는 %d회\n", answer, count);
+  return 0;
+}
