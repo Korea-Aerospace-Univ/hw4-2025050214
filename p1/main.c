d86812b50d73a4a41eff4b8e5f8549a0913d70a6
2025050214 <jeejee637@kau.kr>
2026-04-15 15:42:59 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -12,17 +12,14 @@ int main () {
 
     if (num > answer) {
       printf("%d>?\n", num);
-      printf("%d 보다 낮습니다\n", num);
     }
     else if (num < answer) {
       printf("%d<?\n", num);
-      printf("%d 보다 높습니다\n", num);
     }
     else {
       printf("%d==?\n", num);
-      printf("%d 정답입니다\n", num);
     }
   } while (num != answer);
-  printf("%d ↦ 시도횟수는 %d회\n", answer, count);
+  printf("%d\n", answer, count);
   return 0;
 }
