5d362b8f2b1462bef45814e128f9ae9b1626a53b
2025050214 <jeejee637@kau.kr>
2026-04-15 16:19:22 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main () {
+  int N;
+  char ch;
+
+  int a = 0, am = 0;
+  int b = 0, bm = 0;
+
+  scanf("%d", &N);
+
+  for (int i=0; i<N; i++){
+    scanf("%c", &ch);
+
+    if (ch >= 'a' && ch <= 'z'){
+      a++;
+      if (a > am) am = a;
+    }
+    else if (ch >= '0' && ch <='9'){
+      b++;
+      a = 0;
+      if (b > bm) bm = b;
+    }
+    else {
+      a = 0;
+      b = 0;
+    }
+  }
+  printf("%d\n", am)
+  printf("%d\n", bm)
+
+  return 0;
+}
